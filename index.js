1d1d84a76321deb299f1447b6287802c5d971ff9
Pavan Saini <ps9115981@gmail.com>
2021-11-21 15:45:58 +0530

    first commit

--- index.js
+++ index.js
@@ -23,9 +23,9 @@ app.use(morgan("tiny"));
 //app.use(authjwt());
 app.use(authJwt());
 app.use(errorHandler);
-const api = process.env.API_URL;
-app.use(`${api}/users`, usersRoutes);
-app.use(`${api}/blog`, blogRoutes);
+//const api = process.env.API_URL;
+app.use(`/users`, usersRoutes);
+app.use(`/blog`, blogRoutes);
 
 app.listen(port,()=>{
     console.log(`server is running at port no ${port}`);
